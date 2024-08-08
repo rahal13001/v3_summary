c2dac4a7d986bfceb1f73bda18bb6643d08a3bad
rahal13001 <rahal13001@mail.unpad.ac.id>
2024-08-08 16:33:13 +0900

    perbaikan filter IKU

--- app/Filament/Resources/ReportResource.php
+++ app/Filament/Resources/ReportResource.php
@@ -295,15 +295,9 @@ class ReportResource extends Resource
                         );
                 })->indicator('when'),
                 
-                SelectFilter::make('indicators.indicators_id')
-                    // ->relationship('indicators', 'nama_iku')
+                SelectFilter::make('indicators')
+                    ->relationship('indicators', 'nama_iku')
                     ->label('IKU')
-                    ->options(
-                        Indicator::pluck('nama_iku', 'nama_iku')
-                            ->map(function ($namaIku) {
-                                return $namaIku . ' (Tahun ' . Indicator::where('nama_iku', $namaIku)->first()->tahun_iku . ')';
-                            })
-                    )
                     ->preload()
                     ->multiple()
                     ->searchable()
