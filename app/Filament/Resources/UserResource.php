4e73d2641f82f9dc37397860256c44f6c33711e1
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-05-14 16:27:34 +0900

    update view for superadmin

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -46,7 +46,9 @@ class UserResource extends Resource
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\Select::make('roles')
-                    ->relationship('roles', 'name')
+                    ->relationship('roles', 'name', fn ($query) => 
+                        $query->when(!Auth::user()->hasRole('super_admin'), fn ($q) => $q->where('name', '!=', 'super_admin'))
+                    )
                     ->multiple()
                     ->preload()
                     ->searchable(),
