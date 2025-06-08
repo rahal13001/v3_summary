4a1684ea3621bb383e22b5eba2e74382e7475fad
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-08 23:33:53 +0900

    update : restrict allowed origins in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://arsip.timurbersinar.com/*',
+        'https://www.arsip.timurbersinar.com'
+    ],
 
     'allowed_origins_patterns' => [],
 
